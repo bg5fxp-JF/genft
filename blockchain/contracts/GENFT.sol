9f7f87a45dd60bde41982141ea51c378f40a3fbb
bg5fxp-JF <jacobfecunda@outlook.com>
2023-12-30 12:49:36 +0000

    modified nft

--- blockchain/contracts/GENFT.sol
+++ blockchain/contracts/GENFT.sol
@@ -3,25 +3,21 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 error NotEnoughFunds();
 error MaxNFTsMinted();
 
-contract GENFT is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
+contract GENFT is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Pausable, Ownable {
     uint256 private _nextTokenId;
     uint256 public constant MINT_FEE = 0.01 ether;
     uint256 public immutable i_maxSupply;
-    address public immutable i_owner;
 
-    constructor(uint256 _maxSupply) ERC721("GENFT", "GNT") Ownable(i_owner) {
+    constructor(address initalOwner, uint256 _maxSupply) ERC721("GENFT", "GNT") Ownable(initalOwner) {
         i_maxSupply = _maxSupply;
-        i_owner = msg.sender;
-    }
-
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://firebasestorage.googleapis.com/v0/b/genft-7f0a3.appspot.com/o/metadata%2F";
     }
 
     function pause() public onlyOwner {
@@ -36,7 +32,15 @@ contract GENFT is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
         if (msg.value != MINT_FEE) revert NotEnoughFunds();
         if (totalSupply() >= i_maxSupply) revert MaxNFTsMinted();
         uint256 tokenId = _nextTokenId++;
+        string memory _tokenURI = string(
+            abi.encodePacked(
+                "https://firebasestorage.googleapis.com/v0/b/genft-7f0a3.appspot.com/o/metadata%2F",
+                Strings.toString(tokenId),
+                ".json?alt=media"
+            )
+        );
         _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
     }
 
     // The following functions are overrides required by Solidity.
@@ -53,7 +57,16 @@ contract GENFT is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
         super._increaseBalance(account, value);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
         return super.supportsInterface(interfaceId);
     }
 }
