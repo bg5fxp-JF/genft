8b50238b3f35c2d39ba1a16e133f29272703ea89
bg5fxp-JF <jacobfecunda@outlook.com>
2023-12-31 13:07:44 +0000

    small changes

--- blockchain/contracts/TokGen.sol
+++ blockchain/contracts/TokGen.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TokGen is ERC20 {
+contract Tokgen is ERC20 {
     constructor() ERC20("TokGen", "GEN") {
         _mint(msg.sender, 10000 * 10 ** decimals());
     }
