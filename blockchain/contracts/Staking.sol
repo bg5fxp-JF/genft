6f10c7a39648d978438e857df5a5ee6cb9d78260
bg5fxp-JF <jacobfecunda@outlook.com>
2024-01-02 13:31:52 +0000

    changed compiler version

--- blockchain/contracts/Staking.sol
+++ blockchain/contracts/Staking.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts/interfaces/IERC721.sol";
