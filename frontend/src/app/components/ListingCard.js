2d1e005c33c6faaae8da04f29e4dccd88a838515
bg5fxp-JF <jacobfecunda@outlook.com>
2024-01-05 14:23:56 +0000

    added dynamic listings

--- frontend/src/app/components/ListingCard.js
+++ frontend/src/app/components/ListingCard.js
@@ -4,10 +4,12 @@ import Image from "next/image";
 import Link from "next/link";
 import { FaExternalLinkAlt } from "react-icons/fa";
 
-export default function ListingCard() {
+export default function ListingCard({ uri, tokenId, img, desc, owner }) {
 	function formatAddress(address) {
+		if (address == "You") return address;
 		return address.slice(0, 20) + "..." + address.slice(address.length - 4);
 	}
+
 	return (
 		<motion.div
 			initial={{ opacity: 0, scale: 2 }}
@@ -17,31 +19,27 @@ export default function ListingCard() {
 			className="flex justify-between items-center max-w-[1440px] w-full  my-3  py-4 px-3 bg-bg3 backdrop-filter backdrop-blur-sm bg-opacity-10 rounded-lg text-txt1  "
 		>
 			<div className="flex w-full gap-x-2 items-center text-txt2 ">
-				<p className="w-10% sm:w-[5%]">1</p>
+				<p className="w-10% sm:w-[5%]">{tokenId}</p>
 				<div className="flex items-center gap-x-2 w-[50%]">
 					<Image
-						src="https://img.freepik.com/free-photo/view-funny-monkey-with-sunglasses_23-2150758372.jpg?uid=R131559868&semt=ais_ai_generated"
+						src={img}
 						alt=""
 						width={30}
 						height={30}
 						className="object-cover w-12 h-12 rounded"
 					/>
 
-					<p className=" truncate">
-						Coolest monkey to ever live in the world galaxy and all that
-					</p>
+					<p className=" truncate">{desc}</p>
 				</div>
-				<p className="w-[30%] truncate">
-					{formatAddress("0xcc6cfe1a015ecce15176bfabaa2473728da0898f")}
-				</p>
+				<p className="w-[30%] truncate">{formatAddress(owner)}</p>
 				<Link
-					href="/"
+					href={uri}
 					className="hidden w-[10%] text-primary1 transition-all hover:underline md:flex"
 				>
 					View NFT
 				</Link>
 				<Link
-					href="/"
+					href={uri}
 					className="flex w-[10%] text-primary1 transition-all hover:underline md:hidden"
 				>
 					<FaExternalLinkAlt />
--- frontend/src/app/components/Listings.js
+++ frontend/src/app/components/Listings.js
@@ -1,11 +1,80 @@
+"use client";
+import { useEffect, useState } from "react";
 import ListingCard from "./ListingCard";
+import { useAccount, useContractRead, useNetwork } from "wagmi";
+import { genft_contractAddresses } from "../constants/contracts";
+import { genft_abi } from "../constants/abis";
+import { readContract } from "@wagmi/core";
 
 export default function Listings() {
+	const { address, isConnected } = useAccount();
+	const { chain } = useNetwork();
+	const [nfts, setNfts] = useState([]);
+
+	const chainId = isConnected ? chain.id : 0;
+
+	const genftAddress =
+		chainId in genft_contractAddresses
+			? genft_contractAddresses[chainId][0]
+			: null;
+
+	const { data: totalNfts } = useContractRead({
+		address: genftAddress,
+		abi: genft_abi,
+		functionName: "totalSupply",
+		watch: true,
+	});
+
+	useEffect(() => {
+		let _nfts = [];
+		async function getMetadata(uri, owner) {
+			const response = await fetch(uri);
+			const metadata = await response.json();
+			metadata.uri = uri;
+			metadata.owner = owner;
+			_nfts.push(metadata);
+			setNfts(_nfts);
+		}
+
+		async function checkOwner(id) {
+			if (totalNfts > 0) {
+				let owner = await readContract({
+					address: genftAddress,
+					abi: genft_abi,
+					functionName: "ownerOf",
+					args: [id],
+				});
+
+				if (owner == address) {
+					owner = "You";
+				}
+
+				const uri = `https://firebasestorage.googleapis.com/v0/b/genft-7f0a3.appspot.com/o/metadata%2F${id}.json?alt=media`;
+				getMetadata(uri, owner);
+			}
+		}
+
+		for (let i = 0; i < totalNfts; i++) {
+			checkOwner(i);
+		}
+	}, [isConnected, address, totalNfts]);
+
+	useEffect(() => {
+		setNfts([...nfts]);
+	}, [nfts.length]);
+
+	function extractTokenId(str) {
+		const regex = /#(\d+)/;
+		const matches = str.match(regex);
+
+		return matches ? parseInt(matches[1], 10) : null;
+	}
+
 	return (
 		<section className="relative  flex flex-col gap-y-5 items-center w-full max-w-[1440px] min-h-screen gap-8  mx-auto px-6  bg-bg1 sm:px-16 ">
 			<h3 className=" mt-24 text-3xl text-txt1 font-semibold text-center md:text-6xl ">
-				Curre<span className="text-primary1">n</span>t Listi
-				<span className="text-primary1">n</span>gs
+				Mi<span className="text-primary1">n</span>ted
+				<span className="text-primary1"> N</span>FTs
 			</h3>
 			<div className="w-full ">
 				<div className="flex w-full gap-x-2 px-3 text-txt2 ">
@@ -16,21 +85,27 @@ export default function Listings() {
 				</div>
 				<hr className="w-full" />
 			</div>
-			<ListingCard />
-			<ListingCard />
-			<ListingCard />
-			<ListingCard />
-			<ListingCard />
-			<ListingCard />
-			<ListingCard />
-			<ListingCard />
-			<ListingCard />
-			<ListingCard />
-			<ListingCard />
-			<ListingCard />
-			<ListingCard />
-			<ListingCard />
-			<ListingCard />
+			{isConnected ? (
+				nfts.map(({ name, image, description, uri, owner }) => {
+					const tokenId = extractTokenId(name);
+					return (
+						<ListingCard
+							key={tokenId}
+							uri={uri}
+							tokenId={tokenId}
+							img={image}
+							desc={description}
+							owner={owner}
+						/>
+					);
+				})
+			) : (
+				<div className="flex items-center justify-center max-w-[1440px] w-full min-h-[300px]">
+					<h3 className="text-3xl text-txt1 font-semibold text-center md:text-4xl ">
+						Co<span className="text-primary1">nn</span>ect Wallet To View
+					</h3>
+				</div>
+			)}
 		</section>
 	);
 }
